32ed5b3f0f16a54586dd1bff275b6850c139c50b
Khanyie1 <khanyisanimajozi98@gmail.com>
2024-10-16 13:44:51 +0100

    Deployment set up

--- express.js
+++ express.js
@@ -1,29 +1,29 @@
-import express from 'express';
-import cors from 'cors';
-import path from 'path';
-import { fileURLToPath } from 'url';
+// import express from 'express';
+// import cors from 'cors';
+// import path from 'path';
+// import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
-const app = express();
-app.use(cors());
+// const app = express();
+// app.use(cors());
 
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'UniApplyBot', 'index.html'));
-});
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'UniApplyBot', 'index.html'));
+// });
 
-app.get("/api/deployment", function(req, res) {
-    return res.json({ 
-        status: "success", 
-        message: "Your deployment was successful!", 
-        timestamp: new Date() 
-    });
-});
+// app.get("/api/deployment", function(req, res) {
+//     return res.json({ 
+//         status: "success", 
+//         message: "Your deployment was successful!", 
+//         timestamp: new Date() 
+//     });
+// });
 
-const PORT = process.env.PORT || 1100;
-app.listen(PORT, function() {
-    console.log(`Your application is running on port: ${PORT}`);
-});
+// const PORT = process.env.PORT || 1100;
+// app.listen(PORT, function() {
+//     console.log(`Your application is running on port: ${PORT}`);
+// });
